c3e47355489b8cc8ece8e213ac613ae325cf77fc
Falconflyer7 <mfish3@uccs.edu>
2022-12-05 18:12:26 -0700

    MFUpdates
    -Work on interface

--- FileEncRepo/src/examples/TestMain.java
+++ FileEncRepo/src/examples/TestMain.java
@@ -1,11 +1,34 @@
 package examples;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 public class TestMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Branch testing");
-		System.out.println("test diff");
+
+
+		TestMain main = new TestMain();
+		main.example2();
+
+
 	}
+
+	private void example2() {
+
+		try {
+			String key = "mypassword"; // needs to be at least 8 characters for DES
+
+			FileInputStream fis = new FileInputStream("original.txt");
+			FileOutputStream fos = new FileOutputStream("encrypted.txt");
+			CipherFileExample2.encrypt(key, fis, fos);
+
+			FileInputStream fis2 = new FileInputStream("encrypted.txt");
+			FileOutputStream fos2 = new FileOutputStream("decrypted.txt");
+			CipherFileExample2.decrypt(key, fis2, fos2);
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+	}	
 
 }
